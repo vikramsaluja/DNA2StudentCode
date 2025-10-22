2c01a5124855220c235eb4ab0addf6bb37e5dcfc
vikramsaluja <vikram.saluja@menloschool.org>
2025-10-22 09:57:01 -0700

    Initialized variables and hash

--- src/DNA.java
+++ src/DNA.java
@@ -12,10 +12,27 @@
 
 public class DNA {
 
-    /**
-     * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
-     */
     public static int STRCount(String sequence, String STR) {
+        int dna = sequence.length();
+        int str = STR.length();
+
+        // If the str is longer than dna sequence, then return 0
+        if(str > dna){
+            return 0;
+        }
+
+        int radix = 256;
+        long p = 54321102419L;
+
+        long strHash = 0;
+        long dnaHash = 0;
+        long RM = 1;
+
+        for(int i = 1; i <= str -1; i++){
+            RM = (RM * radix) % p;
+        }
+
+
 
         return 0;
     }
